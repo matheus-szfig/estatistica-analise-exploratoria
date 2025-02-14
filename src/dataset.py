e7a0ea076a61cbbf57e9ae25a12703be15ed6644
ennyagoc <ennyagomesoc@gmail.com>
2025-02-13 22:13:09 -0300

    Novodataset

--- src/dataset.py
+++ src/dataset.py
@@ -4,30 +4,32 @@ import pandas as pd
 from os import path, listdir, getcwd
 
 def get_data_frame () -> DataFrame:
-  dataset_path = path.join(getcwd(), 'datasets/combustiveis.csv')
+  dataset_path = path.join(getcwd(), 'datasets/dados_aereos.csv')
   dataset_path = path.normpath(dataset_path)
 
   df = read_csv(dataset_path, sep=';')
-  df.rename(columns={ 'Estado - Sigla': 'Estado', 'Regiao - Sigla': 'Regiao' }, inplace=True)
+  print(df)
   
-  # Dropar a coluna 'Valor de Compra' pois ela está toda em branco
-  df.drop('Valor de Compra', axis=1, inplace=True)
+  # df.rename(columns={ 'Estado - Sigla': 'Estado', 'Regiao - Sigla': 'Regiao' }, inplace=True)
+  
+  # # Dropar a coluna 'Valor de Compra' pois ela está toda em branco
+  # df.drop('Valor de Compra', axis=1, inplace=True)
 
-  # Processar os dados e dar replace em valores
-  df.replace({
-    'Valor de Venda': { ',': '.' },
-    'Regiao': {
-        '^N$':'Norte',
-        '^NE$': 'Nordeste',
-        '^CO$':'Centro-Oeste',
-        '^SE$': 'Sudeste',
-        '^S$': 'Sul'
-    }
-  }, regex=True, inplace=True)
+  # # Processar os dados e dar replace em valores
+  # df.replace({
+  #   'Valor de Venda': { ',': '.' },
+  #   'Regiao': {
+  #       '^N$':'Norte',
+  #       '^NE$': 'Nordeste',
+  #       '^CO$':'Centro-Oeste',
+  #       '^SE$': 'Sudeste',
+  #       '^S$': 'Sul'
+  #   }
+  # }, regex=True, inplace=True)
 
-  # Transformar a coluna 'Valor de Venda' em numérico
-  df['Valor de Venda'] = pd.to_numeric(df['Valor de Venda'])
-  df['Data da Coleta'] = pd.to_datetime(df['Data da Coleta'], format='%d/%m/%Y')
-  df.sort_values(by=['Data da Coleta'],inplace=True)
+  # # Transformar a coluna 'Valor de Venda' em numérico
+  # df['Valor de Venda'] = pd.to_numeric(df['Valor de Venda'])
+  # df['Data da Coleta'] = pd.to_datetime(df['Data da Coleta'], format='%d/%m/%Y')
+  # df.sort_values(by=['Data da Coleta'],inplace=True)
 
   return df
--- src/main.py
+++ src/main.py
@@ -5,26 +5,26 @@ from plot import plot_scatter_date_price
 def main():
     df = get_data_frame()
 
-    mean = df['Valor de Venda'].mean()
-    median = df['Valor de Venda'].median()
-    mode = df['Valor de Venda'].mode()
+    # mean = df['Valor de Venda'].mean()
+    # median = df['Valor de Venda'].median()
+    # mode = df['Valor de Venda'].mode()
 
-    variance = df['Valor de Venda'].var()
+    # variance = df['Valor de Venda'].var()
 
-    standard_deviation = df['Valor de Venda'].std()
-    var_coef = (standard_deviation / mean) / 100
+    # standard_deviation = df['Valor de Venda'].std()
+    # var_coef = (standard_deviation / mean) / 100
 
-    quartile25 = df['Valor de Venda'].quantile(0.25)
-    quartile50 = df['Valor de Venda'].quantile(0.5)
-    quartile75 = df['Valor de Venda'].quantile(0.75)
+    # quartile25 = df['Valor de Venda'].quantile(0.25)
+    # quartile50 = df['Valor de Venda'].quantile(0.5)
+    # quartile75 = df['Valor de Venda'].quantile(0.75)
 
-    max = df['Valor de Venda'].max()
-    min = df['Valor de Venda'].min()
+    # max = df['Valor de Venda'].max()
+    # min = df['Valor de Venda'].min()
 
-    corr = df[['Valor de Venda', 'Data da Coleta']].corr(method='pearson').loc['Valor de Venda', 'Data da Coleta']
+    # corr = df[['Valor de Venda', 'Data da Coleta']].corr(method='pearson').loc['Valor de Venda', 'Data da Coleta']
 
-    # df = pipeline_run(df)
-    df = plot_scatter_date_price(df)
+    # # df = pipeline_run(df)
+    # df = plot_scatter_date_price(df)
 
 if __name__ == "__main__":
     main()
